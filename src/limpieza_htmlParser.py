1cc678cbd650cb9dbad4fbad92ad87bf0e4e3448
hamer franklin jara ocas <franklinh.jara.o@gmail.com>
2022-07-12 01:54:12 -0500

    codigo de extraccion de header de cv  e implementacion de bloques incompleto

--- src/limpieza_htmlParser.py
+++ src/limpieza_htmlParser.py
@@ -3,10 +3,13 @@ from calendar import c
 import re
 import numpy as np
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import requests
 
 
 
-ficher = open('html_limpio/23.html','r', encoding='utf-8')
+ruta = 'html_limpio/25.html'
+ficher = open(ruta,'r', encoding='utf-8')
 cadena  = ficher.read()
 lista = []
 l = []
@@ -26,27 +29,91 @@ class MyHTMLParser(HTMLParser):
 
 parser = MyHTMLParser()
 parser.feed(cadena)
-ca = np.array(lista)
-unique, count = np.unique(ca,return_counts =True)
-l.append(unique)
-l1.append(count)
-print("Tamaño de fuentes en el documento :",l[0])
-print("Frecuencia de los tamaños de fuentes:", l1[0])
-print("la media de las fuentes es: ",int(np.mean(l)+1))
 
 
+
+#funcion que realiza un conteo de las frecuecia de repeticion de tamaño de fuente 
+def conteo_size(lista):
+    ca = np.array(lista)
+    unique, count = np.unique(ca,return_counts =True)
+    l.append(unique)
+    l1.append(count)
+    
+    print("Tamaño de fuentes en el documento :",l[0])
+    print("Frecuencia de los tamaños de fuentes:", l1[0])
+    print("la media de las fuentes es: ",int(np.mean(l)+1))
+    return  l
+
+l = conteo_size(lista)
+print(l1[0])
+frec = []
+size = []
+#funcion que elimina  no claifica las palabras que no se repiten mas de dos veces con el fin de tener valores mas exactos 
+# al momento de encontrar etiquetas de como los son skill, profile, project, etc
+def arreglos(l,l1):
+    size =l[0]
+    frec = l1[0]
+    i =0 
+    while i <len(frec):
+        if frec[i]<=3:
+            frec = np.delete(frec,i)
+            size = np.delete(size,i)
+            print("se elimino la posicion ",i)
+            print("h",frec)
+            print("tamaños",size)
+            i = 0
+        else:
+            i =1+i
+    return np.max(size),frec,size
+
+valor,array_frec,array_size =arreglos(l,l1)
+
+valor = valor
 #buscar dentro de cada etiqueta con con una fuente mayor a la media y alamacenarlo en un array
-class Busqueda(HTMLParser):
-
-    def handle_starttag(self, tag, attrs):
-        #..... 
-        return attrs
-        
+class header_bloques(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.data= []
+        self.capture =False
+    def handle_starttag(self,tag,attrs):
+        if tag in ('span'):
+            if len(attrs)!=0:
+                for i in attrs:
+                    if 'font-size:'+str(valor)+'px' in i[1]:
+                        self.capture=True
+    def handle_endtag(self, tag):
+        if tag in ('span'):
+            self.capture=False
     def handle_data(self, data):
-        # .....
-        return data
-# pa =Busqueda()
-# pa.feed(cadena)
+        if self.capture:
+            self.data.append(data)
+
+parserr = header_bloques()
+parserr.feed(cadena)
+print(parserr.data)
+
+print(array_size)
+
+
+class body_bloques(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.data= []
+        self.capture =False
+    def handle_starttag(self,tag,attrs):
+        if tag in ('span'):
+            if len(attrs)!=0:
+                for i in attrs:
+                    
+                    if 'font-size:'+str(valor)+'px' in i[1]:
+                        self.capture=True
+    def handle_endtag(self, tag):
+        if tag in ('span'):
+            self.capture=False
+    def handle_data(self, data):
+        if self.capture:
+            self.data.append(data)
+
 
 
 #Grafica la distribucion del tamaño de fuente
